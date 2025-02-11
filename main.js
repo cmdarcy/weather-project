db66c5fc5319739751542417f2b883cd86a892a8
Christopher d'Arcy <cmdarcy89@gmail.com>
2025-02-11 15:11:46 -0500

    add form structure and submit logic

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
+const form = document.getElementsByTagName("form")[0];
 
+form.addEventListener("submit", (e) => {
+	e.preventDefault();
+	const input = document.querySelector(".input-control").value;
+	document.querySelector(".input-control").value = "";
+});
